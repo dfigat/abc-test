099b44721ce42ba44a5d32615b94316dbfbc762d
Daniel Figat <danielfigat@gmail.com>
2017-03-06 10:16:58 +0100

    resolve #2 issue

---
+++ test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "# abc-test" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/dfigat/abc-test.git
+git push -u origin master
